65325315df43d7be8cac1244023c88be307638d7
Abdurashid <coderkamoldinov1@gmail.com>
2024-10-08 18:48:51 +0500

    added file

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User, UserSchema } from './entities/user.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({}),
   ],
   controllers: [UserController],
   providers: [UserService],
